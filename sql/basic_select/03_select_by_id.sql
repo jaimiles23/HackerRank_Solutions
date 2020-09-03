f4fa71b1a9d71fd7d9de281cc5d4123554ad259a
Jai <jaimiles23@gmail.com>
2020-09-03 12:31:04 -0700

    init

---
+++ sql/basic_select/03_select_by_id.sql
@@ -0,0 +1,16 @@
+/**
+ * @author [Jai Miles]
+ * @email [jaimiles23@gmail.com]
+ * @create date 2020-09-03 12:30:17
+ * @modify date 2020-09-03 12:30:17
+ * @desc [
+     Solution to Select by ID: https://www.hackerrank.com/challenges/select-by-id/problem
+ ]
+ */
+
+SELECT
+    *
+FROM
+    CITY
+WHERE
+    ID = 1661
