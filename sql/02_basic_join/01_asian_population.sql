17a5bc331d64a69d759a2902f7e47159c56c99dd
Jai <jaimiles23@gmail.com>
2020-09-05 12:33:50 -0700

    init

--- sql/02_basic_join/01_asian_population.sql
+++ sql/02_basic_join/01_asian_population.sql
@@ -6,5 +6,30 @@
  * @desc [
     Solution to Asian Population:
     https://www.hackerrank.com/challenges/asian-population/problem?isFullScreen=true
+
+    NOTES:
+    - Requirements to understand the metrics desired (SUM)
+    - left join uses the left table as the FROM, and the right table is after left join.
  ]
  */
+
+/*
+Requirements:
+    - SUM populations
+    - WHERE contintent is ASIA
+
+NOTES:
+    - JOIN CITY.CountryCode and COUNTRY.Code
+*/
+
+SELECT
+    SUM(CITY.POPULATION)
+FROM
+    CITY
+LEFT JOIN COUNTRY ON
+    CITY.COUNTRYCODE = COUNTRY.CODE
+WHERE
+    COUNTRY.CONTINENT = 'Asia'
+    
+
+
