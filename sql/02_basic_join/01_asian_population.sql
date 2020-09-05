5fe4be644929ce557a6b6b5648cafdc15fb70b70
Jai <jaimiles23@gmail.com>
2020-09-05 12:04:47 -0700

    Renamed dirs to contain numeric progress

---
+++ sql/02_basic_join/01_asian_population.sql
@@ -0,0 +1,10 @@
+/**
+ * @author [Jai Miles]
+ * @email [jaimiles23@gmail.com]
+ * @create date 2020-09-05 12:02:09
+ * @modify date 2020-09-05 12:02:09
+ * @desc [
+    Solution to Asian Population:
+    https://www.hackerrank.com/challenges/asian-population/problem?isFullScreen=true
+ ]
+ */
