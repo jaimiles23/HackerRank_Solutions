f81983b13b95e5f7deedd6f8a6b93234538b81dd
Jai <jaimiles23@gmail.com>
2020-09-05 18:32:23 -0700

    added CTE solution

---
+++ sql/02_basic_join/08_contest_leaderboard.sql
@@ -0,0 +1,53 @@
+/**
+ * @author [Jai Miles]
+ * @email [jaimiles23@gmail.com]
+ * @create date 2020-09-05 18:30:57
+ * @modify date 2020-09-05 18:30:57
+ * @desc [
+    Solution to Contest Leaderboard:
+    https://www.hackerrank.com/challenges/contest-leaderboard/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+     
+Requirements:
+    - Columns: hacker_id, name, total_score
+    - Order by:
+        - Score, Descending
+        - hacker_id
+    - exclude scrores of 0
+
+Process Notes:
+    - Hackers may have made multiple submissions. Take MAX of their submissions
+    - Create a CTE of the top scores and then pull high_scores from that CTE.
+ ]
+ */
+
+/* Create CTE with MAX of submissions? */
+WITH top_scores AS (
+    SELECT
+        hacker_id,
+        challenge_id,
+        MAX(score) AS high_score
+    FROM
+        Submissions
+    GROUP BY
+        hacker_id, challenge_id
+)
+
+SELECT
+    h.hacker_id,
+    h.name,
+    SUM(t.high_score) AS total_score
+FROM
+    Hackers AS h
+INNER JOIN top_scores AS t
+    ON h.hacker_id = t.hacker_id
+GROUP BY
+    h.hacker_id, 
+    h.name
+HAVING
+    SUM(t.high_score) > 0
+ORDER BY
+    total_score DESC,
+    h.hacker_id
+;
+
