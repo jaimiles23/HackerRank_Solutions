081bebedb37f99844c6eeae95c124901780d2132
Jai <jaimiles23@gmail.com>
2020-09-06 17:19:38 -0700

    init without solution

---
+++ sql/01_basic_select/06_japanese_cities'_names.sql
@@ -0,0 +1,13 @@
+/**
+ * @author [Jai Miles]
+ * @email [jaimiles23@gmail.com]
+ * @create date 2020-09-06 17:18:14
+ * @modify date 2020-09-06 17:18:14
+ * @desc [
+	 Solution to: Japanese Cities' Names
+		https://www.hackerrank.com/challenges/japanese-cities-name
+	
+ ]
+ */
+
+		
