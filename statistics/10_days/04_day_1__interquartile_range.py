9a6f76d1e4653d2a58f62fa4d5c7f870a098df47
Jai <jaimiles23@gmail.com>
2020-09-12 15:51:01 -0700

    init get q range method

--- statistics/10_days/04_day_1__interquartile_range.py
+++ statistics/10_days/04_day_1__interquartile_range.py
@@ -12,6 +12,13 @@ In descriptive statistics, the interquartile range (IQR), is a measure of statis
 being equal to the difference between 75th and 25th percentiles, or between upper and lower quartiles,[1][2] IQR = Q3 −  Q1. 
 
 It is a trimmed estimator, defined as the 25% trimmed range, and is a commonly used robust measure of scale.
+
+## Notes:
+- Do not include median in upper OR lower data sets.
+
+## Process:
+- Create dictionary of values and frequencies before sorting values. 
+	Using a dictionary and sorting the keys has a better practical time complexity than sorting a complete list of values.
  ]
  */
 """
@@ -20,8 +27,7 @@ It is a trimmed estimator, defined as the 25% trimmed range, and is a commonly u
 # Imports
 ##########
 
-from typing import List, Tuple, Union
-
+from typing import Dict, List, Tuple, Union
 
 ##########
 # Input
@@ -40,6 +46,66 @@ def get_input() -> Tuple[int, List[int], List[int]]:
 
 
 ##########
+# Create list of elements
+##########
+def create_val_freq_dict(nums: List[int], freq: List[int]) -> Dict[int, int]:
+	"""Creates a dictionary of numbers & their frequencies in the list."""
+	val_freq_dict = dict()
+	for i in range(len(nums)):
+		val_freq_dict[ nums[i]] = freq[i]
+	
+	assert sum(val_freq_dict.values()) == sum(freq)	## check that no duplicate keys in nums
+	return val_freq_dict
+
+
+def create_total_nums(val_freq_dict: Dict[int, int]) -> List[int]:
+	"""Creates list of values from dictionary with values and frequencies."""
+	total_nums = []
+	for k in sorted(val_freq_dict.keys()):
+		total_nums += [int(k) for k in range(val_freq_dict[k])]
+	return total_nums
+
+
+##########
+# Median
+##########
+
+def get_quantile_range(n: int, nums: List[int], quantile: int) -> List[int]:
+	"""Returns list of integers for the specified quantile range.
+	
+	n: int, length of list
+	nums: list of ints
+	median_index: Union[float, int]. Index of the median
+	quantile: int, what quantile to return
+	"""
+	bound = 0 if quantile == 1 else n
+
+	index, r = divmod(n)
+	if r and quantile == 3:
+		index += 1
+	
+	b1, b2 = sorted([bound, index])
+	return nums[b1: b2]
+
+
+
+	
+	
+	
+
+
+def calc_median(nums: List[int], median_index: Union[float, int):
+	pass
+
+
+
+##########
+# Quartiles
+##########
+
+
+
+##########
 # Main
 ##########
 
@@ -49,4 +115,3 @@ def main():
 
 if __name__ == "__main__":
 	main()
-
