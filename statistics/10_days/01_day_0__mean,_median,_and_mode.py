feeaef200cc1e7a1493889d8f09b1e3005930bdc
Jai <jaimiles23@gmail.com>
2020-09-11 17:18:04 -0700

    added divmod method

--- statistics/10_days/01_day_0__mean,_median,_and_mode.py
+++ statistics/10_days/01_day_0__mean,_median,_and_mode.py
@@ -5,12 +5,12 @@
  * @create date 2020-09-11 16:24:44
  * @modify date 2020-09-11 16:24:44
  * @desc [Solution to: Day 0: Mean, Median, and Mode
-        https://www.hackerrank.com/challenges/s10-basic-statistics/problem]
+		https://www.hackerrank.com/challenges/s10-basic-statistics/problem]
 
 NOTE:
-    - Print on separate lines (mean, median, mode)
-    - Scale of 1 decimal place
-    - With multiple mode values, select min.
+	- Print on separate lines (mean, median, mode)
+	- Scale of 1 decimal place
+	- With multiple mode values, select min.
  */
 """
 
@@ -19,14 +19,14 @@ NOTE:
 ##########
 
 from typing import List, Union
-from collections import Counter            # used to count instances for mode
+from collections import Counter			# used to count instances for mode
 
 
 ##########
 # Constants
 ##########
 
-SCALE = 1            # scale refers to number of decimal places
+SCALE = 1			# scale refers to number of decimal places
 
 
 ##########
@@ -43,35 +43,36 @@ nums = sorted(int(num) for num in input().split())
 ##########
 
 def calc_mean(nums: List[int], n: int) -> Union[int, float]:
-    """Returns mean of nums list with n elements."""
-    return format_scale( sum(nums) / n)
+	"""Returns mean of nums list with n elements."""
+	return format_scale( sum(nums) / n)
 
 
 def calc_median(nums: List[int], n: int) -> Union[int, float]:
-    """Returns median of nums list with n elements."""
-    if n % 2:
-        median = nums[n//2]
-    else:
-        median = calc_mean( nums[n//2 - 1: n//2 + 1], 2)
-    return format_scale(median)
+	"""Returns median of nums list with n elements."""
+	mid, is_odd = divmod(nums, 2)
+	if is_odd:
+		median = nums[mid]
+	else:
+		median = calc_mean( nums[mid - 1, mid + 1], 2)
+	return format_scale(median)
 
 
 def calc_mode(nums: List[int], n: int) -> int:
-    """Returns mode of nums list.
-    
-    Uses auxiliary get_mode_ties func.
-    """
-    def get_mode_ties(num_counts: dict, max_freq: int) -> int:
-        """Generator function to return contenders for mode in dict."""
-        for num, count in num_counts.items():
-            if count == max_freq:
-                yield num
+	"""Returns mode of nums list.
+	
+	Uses auxiliary get_mode_ties func.
+	"""
+	def get_mode_ties(num_counts: dict, max_freq: int) -> int:
+		"""Generator function to return contenders for mode in dict."""
+		for num, count in num_counts.items():
+			if count == max_freq:
+				yield num
 
 
-    num_counts = Counter(nums)
-    max_freq = num_counts[ max( num_counts, key = num_counts.get)]
+	num_counts = Counter(nums)
+	max_freq = num_counts[ max( num_counts, key = num_counts.get)]
 
-    return min(list(get_mode_ties(num_counts, max_freq)))
+	return min(list(get_mode_ties(num_counts, max_freq)))
 
 
 ##########
@@ -79,10 +80,10 @@ def calc_mode(nums: List[int], n: int) -> int:
 ##########
 
 def format_scale(num: Union[int, float]) -> float:
-    """Returns float number ."""
-    if isinstance(num, int):
-        return num
-    return round(num, SCALE)
+	"""Returns float number ."""
+	if isinstance(num, int):
+		return num
+	return round(num, SCALE)
 
 
 ##########
@@ -90,14 +91,14 @@ def format_scale(num: Union[int, float]) -> float:
 ##########
 
 def main():
-    measure_central_tendency = (
-        calc_mean,
-        calc_median,
-        calc_mode
+	measure_central_tendency = (
+		calc_mean,
+		calc_median,
+		calc_mode
 	)
-    for measure in measure_central_tendency:
-        print(measure(nums, n))
+	for measure in measure_central_tendency:
+		print(measure(nums, n))
 
 
 if __name__ == "__main__":
-    main()
+	main()
