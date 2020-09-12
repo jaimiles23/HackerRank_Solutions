48036bcd2669e8c9e94df006a5c821c8d827fe28
Jai <jaimiles23@gmail.com>
2020-09-11 17:30:57 -0700

    added outline structure

--- statistics/10_days/02_day_0__weighted_mean.py
+++ statistics/10_days/02_day_0__weighted_mean.py
@@ -1,2 +1,54 @@
-Solution to: Day 0: Weighted Mean
+"""/**
+ * @author [Jai Miles]
+ * @email [jaimiles23@gmail.com]
+ * @create date 2020-09-11 17:23:28
+ * @modify date 2020-09-11 17:23:28
+ * @desc [
+	Solution to: Day 0: Weighted Mean
 		https://www.hackerrank.com/challenges/s10-weighted-mean
+ ]
+ */
+"""
+
+##########
+# Imports
+##########
+
+from typing import List, Union
+
+
+##########
+# Constants
+##########
+
+SCALE = 1	# number of decimals
+
+##########
+# Weighted Mean
+##########
+
+
+
+
+##########
+# Format scale
+##########
+
+def format_scale(num: Union[int, float]) -> Union[int, float]:
+	if isinstance(num, int):
+		return num
+	
+	return round(num, 1)
+
+
+##########
+# Main
+##########
+
+def main():
+	pass
+
+
+if __name__ == "__main__":
+	main()
+
