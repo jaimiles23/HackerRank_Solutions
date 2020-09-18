f09ab7c21cf4a066a59c98b625df233f2273f7c5
Jai <jaimiles23@gmail.com>
2020-09-17 19:11:24 -0700

    u docu

--- statistics/10_days/10_day_3_cards_of_the_same_suit.py
+++ statistics/10_days/10_day_3_cards_of_the_same_suit.py
@@ -1,2 +1,15 @@
-Solution to: Day 3: Cards of the Same Suit
+"""/**
+ * @author [Jai Miles]
+ * @email [jaimiles23@gmail.com]
+ * @create date 2020-09-17 17:11:58
+ * @modify date 2020-09-17 17:11:58
+ * @desc [
+	Solution to: Day 3: Cards of the Same Suit
 		https://www.hackerrank.com/challenges/s10-mcq-5/problem
+
+
+
+ ]
+ */
+
+"""
