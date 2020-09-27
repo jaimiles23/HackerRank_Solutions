87640cc7567770e5187c5e854fc4000d3a8583ea
Jai <jaimiles23@gmail.com>
2020-09-26 18:25:49 -0700

    init

--- statistics/10_days/26_day_8_pearson_correlation_coefficient_ii.py
+++ statistics/10_days/26_day_8_pearson_correlation_coefficient_ii.py
@@ -1,2 +1,42 @@
-Solution to: Day 8: Pearson Correlation Coefficient II
+"""/**
+ * @author [Jai Miles]
+ * @email [jaimiles23@gmail.com]
+ * @create date 2020-09-26 18:07:53
+ * @modify date 2020-09-26 18:25:43
+ * @desc [
+	Solution to: Day 8: Pearson Correlation Coefficient II
 		https://www.hackerrank.com/challenges/s10-mcq-7/problem
+
+## original lines
+line 1: 3x +4y + 8 = 0
+line 2: 4x + 3y + 7 = 0
+
+## Rewritten to regression form:
+line 1:
+	y = (-3x - 8) / 4
+line 2:
+	x = (-3y - 7) / 4
+
+## b values
+b1 = -3/4
+b2 = -3/4
+
+## Know that p = b * x_sd / y_sd from here:
+[tutorial](https://www.hackerrank.com/challenges/s10-least-square-regression-line/tutorial)
+	p1 = b1 * x_std/y_std
+	p2 = b2 * (y_std/x_std)
+
+Thus, 
+	p**2 = b1 * b2
+		= (-3/4) * (-3/4)
+		= 9/16
+		= 3/4
+
+However, by definition, the pearson correlation will have the same slope as the line.
+Thus, the correct answer is -3/4s
+ ]
+ */
+"""
+
+
+
