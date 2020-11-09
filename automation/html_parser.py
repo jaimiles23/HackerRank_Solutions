5f56e36a50d0c6d176ddc9ff8009abc37fe04d09
Jai <jaimiles23@gmail.com>
2020-11-08 21:57:08 -0800

    added loggers

--- automation/html_parser.py
+++ automation/html_parser.py
@@ -15,6 +15,8 @@ from typing import List
 import bs4
 import googlesearch
 
+from logger import logging
+
 
 ##########
 # HTML parser
@@ -47,6 +49,10 @@ class InfoParser(object):
             text (str): Text to parse
             challenge_info (dict): Contains information about challenges. Used for urls to avoid excessive searches.
         """
+        ## Log
+        logging.info(f"Parsing information for {domain} - {sub_domain}, {challenge_num}")
+        logging.debug(f"Parsing text:\n\n{text}\n\n")
+
         ## Directory
         self.domain = domain
         self.sub_domain = sub_domain
@@ -72,12 +78,16 @@ class InfoParser(object):
     def get_challenge_num(self, num) -> int:
         """Sets challenge number."""
         num_zeros = len(self.total) - len(str(num))
-        return '0' * num_zeros + str(num)
+        num = '0' * num_zeros + str(num)
+        logging.debug(f"Challenge num {num}")
+        return num
 
 
     def get_name(self) -> str:
         """Returns name of problem from text."""
-        return self.text[0: self.text.find("\n")]
+        name = self.text[0: self.text.find("\n")]
+        logging.debug(f"Name {name}")
+        return name
 
 
     def get_filename(self) -> str:
@@ -87,16 +97,19 @@ class InfoParser(object):
             special_chars = [' ','<', '>', ':', '"', "'", '/', '\\', '|', '?', '*',]
             for char in special_chars:
                 name = name.replace(char, '')
+            logging.debug(f'No special chars: {name}')
             return name
 
 
         name = remove_special_chars(self.name)
-        return ''.join([
+        filename = ''.join([
             self.num,
             "_",
             name.replace(' ', '_').lower(),
             '.',
             self.filetype])
+        logging.debug(f"Filename: {filename}")
+        return filename
 
 
     def get_difficulty(self) -> str:
@@ -105,22 +118,27 @@ class InfoParser(object):
             self.text.find("\n") + 1 : self.text.find("Max")
         ]
         difficulty = difficulty if difficulty != 'The challenge is not available yet' else 0        # for locked problems
+        logging.debug(f"Difficulty: {difficulty}")
         return difficulty
 
 
     def get_score(self) -> str:
         """Returns score of problem from text."""
         score_str, success_str = "Score: ", "Success"
-        return self.text[
+        score = self.text[
             self.text.find(score_str) + len(score_str) : self.text.find(success_str)
         ]
+        logging.debug(f"Score: {score}")
+        return score
 
 
     def get_rate(self) -> str:
         """Returns success rate of problem from text."""
         rate_str = "Rate: "
-        return self.text[
+        rate = self.text[
             self.text.find(rate_str) + len(rate_str) : ]
+        logging.debug(f"Rate: {rate}")
+        return rate
 
 
     def get_challenge_url(self, challenge_info: dict) -> str:
@@ -141,11 +159,14 @@ class InfoParser(object):
         
         time.sleep(1/5)     # slow HTTP requests
         search_items = "site: hackerrank.com Challenges {}".format(self.name)
-        return googlesearch.lucky(search_items).replace("/forum", "/problem")   # may return forum/problem
+        url = googlesearch.lucky(search_items).replace("/forum", "/problem")   # may return forum/problem
+        logging.debug("URL: {url}")
+        return url
     
 
     def get_github_url(self) -> str:
         """Returns url of github solution from text."""
-        return InfoParser.GITHUB_URL_BASE.format( 
+        github_url = InfoParser.GITHUB_URL_BASE.format( 
             self.domain, self.sub_domain, self.filename)
+        logging.debug(f"github_url: {github_url}")
 
