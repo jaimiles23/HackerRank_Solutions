8c98f95ff84944c81d71052532f38cea041821bd
Jai <jaimiles23@gmail.com>
2020-12-07 08:41:03 -0800

    Solution to 3 - Python: Division

--- python/01_introduction/4_pythondivision.py
+++ python/01_introduction/4_pythondivision.py
@@ -1,1 +1,8 @@
-Solution to [Python: Division](https://www.hackerrank.com/challenges/python-division)
+# Solution to [Python: Division](https://www.hackerrank.com/challenges/python-division)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a // b)
+    print(a / b)
